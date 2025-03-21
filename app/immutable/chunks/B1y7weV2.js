60fdd5134383c134842e21a8b29635022f41fd3b
Luis <lval@users.noreply.github.com>
2025-03-21 01:07:37 -0400

    .

---
+++ app/immutable/chunks/B1y7weV2.js
@@ -0,0 +1,1 @@
+import{g as s}from"./BlSi8SU-.js";const r=()=>"Starter Paks",a=()=>"Paks Iniciales",i=(n={},e={})=>{const t=e.locale??s();return t==="en"?r():t==="es"?a():"packs_title"};export{i as p};
