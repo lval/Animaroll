8bd71e5515af3744fd824b6e9a1c29a5b585be8d
Luis <lval@users.noreply.github.com>
2025-03-22 03:46:36 -0400

    .

---
+++ app/immutable/chunks/Du44hCfn.js
@@ -0,0 +1,1 @@
+import{g as s}from"./RHnUQDhl.js";const r=()=>"Starter Paks",a=()=>"Paks Iniciales",i=(n={},e={})=>{const t=e.locale??s();return t==="en"?r():t==="es"?a():"packs_title"};export{i as p};
